ad0d9946cbc4b82d0a9be0183b6c75509d63b475
Vladimir Ignatov <vladimir.ignatov@childrens.harvard.edu>
2017-06-08 15:41:41 -0400

    Add the authorize-client app

---
+++ authorize-client/README.md
@@ -0,0 +1,36 @@
+# Authorization Example
+
+This simple app is able to identify both the current patient and the current user
+(they might also be the same). Please see the code for details.
+
+
+## Install & Run
+Install NodeJS, go to the app directory and run:
+```sh
+npm i
+npm start
+```
+
+You should see something like
+
+    Starting up http-server, serving ./
+    Available on:
+        http://127.0.0.1:9090
+        http://10.23.49.21:9090
+
+You can stop the server if needed using <kbd>Ctrl+C</kbd>.
+
+Now go the sandbox at https://sandbox.smarthealthit.org/smartdstu3/#/manage-apps
+and `Register New App Manually` using the following data:
+
+    App Type          : Public Client
+    App Name          : Whatever
+    App Launch URI    : http://127.0.0.1:9090/launch.html
+    App Redirect URIs : http://127.0.0.1:9090
+    Patient Scoped App: true (checked)
+    Scopes            : patient/*.read openid profile
+
+When you save the app new `Client Id` will be presented to you. Copy that ClientID,
+open the file `launch.html` and replace the old id (`my_web_app`) with the new one.
+Now you can return to the sandbox, click the **Launch** button below your app, choose
+a patient and see how it works.
---
+++ authorize-client/app.js
@@ -0,0 +1,135 @@
+function urlParam(p) {
+    var query  = location.search.replace(/^\?/, "");
+    var data   = query.split("&");
+    var result = [];
+    var i, item;
+
+    for (i = 0; i < data.length; i++) {
+        item = data[i].split("=");
+        if (item[0] === p) {
+            return decodeURIComponent(item[1].replace(/\+/g, '%20'));
+        }
+    }
+
+    return null;
+}
+
+function getRedirectURI() {
+    return (location.protocol + "//" + location.host + location.pathname)
+        .match(/(.*\/)[^\/]*/)[1];
+}
+
+function refreshApp() {
+    location.href = getRedirectURI();
+}
+
+function initialize(settings) {
+    setSettings({
+        client_id     : settings.client_id,
+        secret        : settings.secret,
+        scope         : settings.scope + " launch",
+        launch_id     : urlParam("launch"),
+        api_server_uri: urlParam("iss")
+    });
+    clearAuthToken();
+    refreshApp();
+}
+
+function completeAuth() {
+    FHIR.oauth2.ready(refreshApp);
+}
+
+function writeData(key, data) {
+    sessionStorage[key] = JSON.stringify(data);
+}
+
+function readData(key) {
+    var data = sessionStorage[key];
+    if (data) {
+        return JSON.parse(data);
+    }
+    return data;
+}
+
+function getSettings() {
+    return readData("app-settings");
+}
+
+function setSettings(data) {
+    writeData("app-settings", data);
+}
+
+function hasAuthToken() {
+    return sessionStorage.tokenResponse !== undefined;
+}
+
+function clearAuthToken() {
+    delete sessionStorage.tokenResponse;
+}
+
+function getHumanName(name) {
+    return name.given.join(" ") + " " + name.family;
+}
+
+function authorize() {
+    var settings = getSettings();
+
+    FHIR.oauth2.authorize({
+        "client": {
+            "client_id": settings.client_id,
+            "scope"    : settings.scope,
+            "launch"   : settings.launch_id
+        },
+        "server": settings.api_server_uri
+    });
+}
+
+function getPatientName() {
+    var ret = $.Deferred();
+
+    FHIR.oauth2.ready(function(smart) {
+        var patient = smart.patient;
+        patient.read().then(function(pt) {
+            ret.resolve(getHumanName(pt.name[0]));
+        }).fail(function() {
+            ret.reject("Could not fetch patient name");
+        });
+    });
+
+    return ret.promise();
+}
+
+function getUserName() {
+    var ret = $.Deferred();
+
+    FHIR.oauth2.ready(function(smart){
+        var user = smart.user;
+        
+        // smart.userId = "Patient/" + smart.userId
+        $.when(user.read())
+        .then(function(pt) {
+            if (pt) {
+                var name;
+                if (pt.resourceType === "Practitioner" || pt.resourceType === "RelatedPerson") {
+                    ret.resolve(getHumanName(pt.name));
+                }
+                else if (pt.resourceType === "Patient") {
+                    ret.resolve(getHumanName(pt.name[0]));
+                }
+                else {
+                    ret.reject("Could not fetch user name");
+                }
+            }
+            else {
+                ret.resolve(pt);
+            }
+        })
+        .fail(function(error) {
+            window.SMART = smart
+            console.log(smart)
+            ret.reject("Could not fetch user name: " + error);
+        });
+    });
+
+    return ret.promise();
+}
