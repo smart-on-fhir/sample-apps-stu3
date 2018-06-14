739c78676bb1ecacb272e2a0e2fa968713e04d70
Vladimir Ignatov <vladimir.ignatov@childrens.harvard.edu>
2018-06-14 15:52:45 -0400

    Added sample CDS Hooks service

---
+++ cds-hooks-service/README.md
@@ -0,0 +1,34 @@
+# CDS Hooks Service
+
+This is a sample CDS Hooks Service that demonstrates how to launch a SMART app from a card in the EHR.
+If you prescribe a medication to patient who is less than 18 years old, a card will be displayed
+containing al link for launching the PGC application. Please see the code for more details.
+
+
+## Install & Run
+
+Install NodeJS, Go to the app directory and run:
+```sh
+npm i
+npm start
+```
+
+You should see something like
+
+    Listening on port 3000!
+
+You can stop the server if needed using <kbd>Ctrl+C</kbd>.
+
+Now that the your cds services server is running, do to the launcher,
+select the "CDS Hooks Service" Launch Type, select a Patient and Provider,
+enter "http://localhost:3000/cds-services" as Discovery Endpoint URL and click
+"Launch". Then in the CDS Hooks Sandbox prescribe whatever medication and if
+the chosen patient is 18 years old or younger you should se a card wit a launch link.
+
+NOTE: Proper patient to launch with would be anybody having vital sing observations,
+conditions and age <= 18 years.
+
+- [Example with young patient](https://launch.smarthealthit.org/?auth_error=&fhir_version_1=r2&fhir_version_2=r3&iss=&launch_cds=1&launch_url=&patient=d0d0cde0-4b21-42f6-9c1e-bfa447d72059&prov_skip_auth=1&prov_skip_login=1&provider=smart-Practitioner-71614502&pt_skip_auth=1&public_key=&sb=&sde=http%3A%2F%2Flocalhost%3A3000%2Fcds-services&token_lifetime=15&user_pt=)
+- [Example with older patient (no card shown)](https://launch.smarthealthit.org/?auth_error=&fhir_version_1=r2&fhir_version_2=r3&iss=&launch_cds=1&launch_url=&patient=smart-1213208&prov_skip_auth=1&prov_skip_login=1&provider=smart-Practitioner-71614502&pt_skip_auth=1&public_key=&sb=&sde=http%3A%2F%2Flocalhost%3A3000%2Fcds-services&token_lifetime=15&user_pt=)
+
+
---
+++ cds-hooks-service/index.js
@@ -0,0 +1,72 @@
+const express    = require("express");
+const cors       = require("cors");
+const bodyParser = require("body-parser");
+const moment     = require("moment");
+
+const app = express();
+
+app.use(express.static("static"));
+app.use(cors({ origin: true, credentials: true }));
+app.use(bodyParser.json());
+
+// list services
+app.get("/cds-services", (req, res) => res.json({
+    services: [
+        {
+            hook: "medication-prescribe",
+            title: "Offer App Upon Prescription",
+            description: "An example of a CDS Service that offers launching a SMART app after a medication is prescribed",
+            "id": "app-launcher",
+            "prefetch": {
+                "patient": "Patient/{{context.patientId}}"
+            }
+        }
+    ]
+}));
+
+app.post("/cds-services/:id", (req, res) => {
+    let out = {};
+    let patient = req.body.prefetch.patient;
+
+    do {
+
+        // If the patient is not pre-fetched don't render a card
+        if (!patient) {
+            break;
+        }
+
+        // If we know that the patient is deceased but we don't know when s(he)
+        // died, then we cannot compute if s(he) is good candidate for a
+        // pediatric app.
+        if (patient.deceasedBoolean) {
+            break;
+        }
+
+        let startDate = moment(patient.birthDate);
+        let endDate   = moment(patient.deceasedDateTime || []);
+        let age       = endDate.diff(startDate, "years");
+
+        // Don't offer to launch a pediatric app for patients older than 19 years
+        if (age > 18) {
+            break;
+        }
+
+        out.cards = [{
+            summary  : "Please launch this app!",
+            indicator: "warning",
+            detail   : '<img src="http://localhost:3000/app.png"/>',
+            links: [
+                {
+                    "label": "Pediatric Growth Charts",
+                    "type" : "smart",
+                    "url"  : "https://sb-apps.smarthealthit.org/apps/growth-chart/launch.html"
+                }
+            ]
+        }];
+    } while( false );
+
+    res.json(out);
+});
+
+
+app.listen(3000, () => console.log("Listening on port 3000!"));
